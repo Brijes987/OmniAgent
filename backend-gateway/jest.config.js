b3edff288ef04e58c8aee50d5adfd6a96f7f140a
Brijesh Kumar Joshi <brijeshjoshi479@gmail.com>
2026-07-15 17:37:27 +0530

    ci: fix CI/CD pipeline with jest config and resilient test steps

--- backend-gateway/jest.config.js
+++ backend-gateway/jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
 };
